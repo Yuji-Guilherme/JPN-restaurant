67f8baf23eaa5144e0cdfcec7d84e459c10e0fae
Guilherme <guilherme.nagasaki@outlook.com>
2023-01-09 18:30:18 -0300

    finished desktop version

--- script/menu-btn.js
+++ script/menu-btn.js
@@ -4,13 +4,26 @@ let navbar = document.getElementById('navbar');
 let active = 'active';
 let show = 'show';
 
-btn.addEventListener('click', () => {
+function closeMenu() {
+    navbar.classList.remove(show);
+}
+
+function toggleMenu(e) {
+    if (e.type === 'touchstart') {e.preventDefault()};
     navbar.classList.toggle(show);
     btn.classList.toggle(active);
-})
+    let navbarOpen = navbar.classList.contains(show);
+    e.currentTarget.setAttribute('aria-expanded', navbarOpen);
+    if (navbarOpen) {
+        e.currentTarget.setAttribute('aria-label', 'close menu');
+    } else {
+        e.currentTarget.setAttribute('aria-label', 'open menu');
+    }
+}
+
+btn.addEventListener('click', toggleMenu)
+btn.addEventListener('touchstart', toggleMenu)
 
 navBtn.forEach((e) => {
-    e.addEventListener('click', () => {
-        navbar.classList.remove(show);
-    })
+    e.addEventListener('click', closeMenu)
 })
